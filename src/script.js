d1b56051dbec34f5ccd12d1541b38fcbeb2f7701
melohugo <hugoq2004@gmail.com>
2024-06-21 21:21:05 -0300

    Adiciona função de nextPokemon e previousPokemon solucionando o desafio

--- src/script.js
+++ src/script.js
@@ -1,23 +1,40 @@
+const MAX = 1292;
+let now = Math.floor(Math.random() * MAX);
+
 //função de mudar imagem pelo id e pela url
 function changeImage(id, url) {
-  document.getElementById(id).src = url;
+    document.getElementById(id).src = url;
 }
 //função de mudar texto pelo id e pelo texto
 function changeText(id, text) {
-  document.getElementById(id).innerText = text;
+    document.getElementById(id).innerText = text;
 }
 
 // Daqui para baixo voce ira escrever
 // o código para resolver o desafio
 
-function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+async function previousPokemon() {
+    now = Math.floor((now - 1) < 0 ? MAX : now - 1);
+    const name = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=' + now + '&limit=1')
+    .then((req) => req.json());
+
+    const info = await fetch(name.results[0].url)
+    .then((req) => req.json());
+
+    changeImage('img_sprite_front_default' , info.sprites.front_default);
+    changeText('name' , info.name);
+    console.log("Pokemon Anterior");
 }
 
-function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+async function nextPokemon() {
+    now = Math.floor((now + 1) % MAX);
+    const name = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=' + now + '&limit=1')
+    .then((req) => req.json());
+
+    const info = await fetch(name.results[0].url)
+    .then((req) => req.json());
+
+    changeImage('img_sprite_front_default' , info.sprites.front_default);
+    changeText('name' , info.name);
+    console.log("Pokemon Seguinte");
 }
