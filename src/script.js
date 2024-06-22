4bd09e4570a857581d22e89267e208962aa6d681
melohugo <hugoq2004@gmail.com>
2024-06-22 20:26:48 -0300

    Refatorando feth da API

--- src/script.js
+++ src/script.js
@@ -1,24 +1,26 @@
 const MAX = 1292;
+const API = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292';
+let pokemons = [];
 let now = Math.floor(Math.random() * MAX);
 
-//função de mudar imagem pelo id e pela url
 function changeImage(id, url) {
     document.getElementById(id).src = url;
 }
-//função de mudar texto pelo id e pelo texto
 function changeText(id, text) {
     document.getElementById(id).innerText = text;
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+async function getPokemons() {
+    let temp = await fetch(API).then((list) => list.json());
+    pokemons = temp.results;
+}
+
+getPokemons();
 
 async function previousPokemon() {
     now = Math.floor((now - 1) < 0 ? MAX : now - 1);
-    const name = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=' + now + '&limit=1')
-    .then((req) => req.json());
 
-    const info = await fetch(name.results[0].url)
+    const info = await fetch(pokemons[now].url)
     .then((req) => req.json());
 
     changeImage('img_sprite_front_default' , info.sprites.front_default);
@@ -28,10 +30,8 @@ async function previousPokemon() {
 
 async function nextPokemon() {
     now = Math.floor((now + 1) % MAX);
-    const name = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=' + now + '&limit=1')
-    .then((req) => req.json());
 
-    const info = await fetch(name.results[0].url)
+    const info = await fetch(pokemons[now].url)
     .then((req) => req.json());
 
     changeImage('img_sprite_front_default' , info.sprites.front_default);
